a9bf00064ff4ebcfa889138a5892424eeb13695d
Utkarsh Sharma <utkarsh@techanicinfotech.com>
2024-06-25 00:22:05 +0530

    Router Subscribed

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
+import authentication from './authentication/authentication'
 
 export default (): Router => {
     const router: Router = Router()
+
+    authentication(router)
 
     return router
 }
